1f38536128dba9ba7b9d50d29ee7852b95f99f28
Vidarshana Govilesh <68025543+vidarshana20@users.noreply.github.com>
2025-11-05 23:15:51 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (71.38%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,12 +1,21 @@
 import math
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        
-
-
             if str1 + str2 != str2 + str1:
                 return ""
             gcd_len = math.gcd(len(str1), len(str2))
             return str1[:gcd_len]
 
 
+# class Solution:
+#     def gcdOfStrings(self, str1: str, str2: str) -> str:
+#         # helper to compute gcd of two numbers manually
+#         def gcd(a, b):
+#             while b:
+#                 a, b = b, a % b
+#             return a
+
+#         if str1 + str2 != str2 + str1:
+#             return ""
+#         gcd_len = gcd(len(str1), len(str2))
+#         return str1[:gcd_len]
