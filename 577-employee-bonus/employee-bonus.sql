1254c34084f149ee34e4f6d11ff53e411a627fbc
Vidarshana Govilesh <68025543+vidarshana20@users.noreply.github.com>
2025-11-15 23:18:01 -0500

    Time: 992 ms (32.12%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -4,13 +4,4 @@
 select e.name, b.bonus from Employee e left join bonus b on e.empId=b.empId where b.bonus<1000 or b.bonus is null;
 
 
--- select 
---     e.name, b.bonus 
--- from 
---     Employee e 
---  join 
---     bonus b 
--- on 
---     e.empId=b.empId and b.bonus<1000;
-
 
